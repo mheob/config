2c252d7d9efa2ebbcd62fa8327042abd983624d0
Alexander Böhm <tools@boehm.work>
2022-09-04 19:46:18 +0200

    feat(eslint): deactivate eslint rule
    Deactivate the `@typescript-eslint/explicit-function-return-type` rule for jsx files.

---
+++ .changeset/healthy-donuts-live.md
@@ -0,0 +1,5 @@
+---
+'@mheob/eslint-config': minor
+---
+
+Deactivate `@typescript-eslint/explicit-function-return-type` rule for jsx files.
--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -14,6 +14,7 @@ module.exports = {
       files: ['*.jsx', '*.tsx'],
       settings: { react: { version: 'detect' } },
       rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
         'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
         'react/jsx-no-useless-fragment': 'warn',
         'react/react-in-jsx-scope': 'off',
