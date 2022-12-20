be52c6f3a6525c7616a0dcb00bb24e2eb9631a0c
Alex Böhm <tools@boehm.work>
2022-12-20 11:12:37 +0100

    feat(prettier)!: remove `@trivago/prettier-plugin-sort-imports`
    "Sorting/moving imports, object keys, class members, JSX keys, CSS properties or anything else..." should not be part of `prettier`.
    See: https://prettier.io/docs/en/rationale.html#what-prettier-is-_not_-concerned-about
    
    BREAKING CHANGE: Prettier will no more sort the imports. Use the `@mheob/eslint-config` for this behavior.

---
+++ .changeset/fair-ants-warn.md
@@ -0,0 +1,5 @@
+---
+'@mheob/prettier-config': major
+---
+
+remove `@trivago/prettier-plugin-sort-imports`
--- packages/prettier-config/src/index.ts
+++ packages/prettier-config/src/index.ts
@@ -1,24 +1,10 @@
-import sortImports from '@trivago/prettier-plugin-sort-imports';
-import type { Config as DefaultConfig } from 'prettier';
-
-interface Config extends DefaultConfig {
-	importOrder?: string[];
-	importOrderCaseInsensitive?: boolean;
-	importOrderParserPlugins?: string[];
-	importOrderSeparation?: boolean;
-	importOrderGroupNamespaceSpecifiers?: boolean;
-	importOrderSortSpecifiers?: boolean;
-}
+import type { Config } from 'prettier';
 
 const options: Config = {
-	plugins: [sortImports],
 	arrowParens: 'always',
 	endOfLine: 'lf',
 	printWidth: 100,
 	proseWrap: 'always',
-	importOrder: ['^node:', '<THIRD_PARTY_MODULES>', '^[./]'],
-	importOrderSeparation: true,
-	importOrderSortSpecifiers: true,
 	singleQuote: true,
 	semi: true,
 	trailingComma: 'all',
