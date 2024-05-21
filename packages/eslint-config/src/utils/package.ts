cdbb079c786a5826213048f2e0bb7ce02177b8ae
Alex Böhm <tools@boehm.work>
2024-05-21 17:23:29 +0200

    chore: fix ci run

--- packages/eslint-config/src/utils/package.ts
+++ packages/eslint-config/src/utils/package.ts
@@ -55,7 +55,6 @@ export async function interopDefault<T>(
 	m: Awaitable<T>,
 ): Promise<T extends { default: infer U } ? U : T> {
 	const resolved = await m;
-
 	return (resolved as any).default || resolved;
 }
 
