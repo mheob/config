62fe6f3b6d24727ffa12672914c79ea6c95ba8d1
Alex Böhm <tools@boehm.work>
2024-08-04 20:57:27 +0200

    ci(eslint): add minify

--- packages/eslint-config/tsup.config.ts
+++ packages/eslint-config/tsup.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
+export default defineConfig(options => ({
 	dts: true,
 	entry: ['src/index.ts'],
-	format: ['esm'],
-});
+	format: ['cjs', 'esm'],
+	minify: !options.watch,
+}));
