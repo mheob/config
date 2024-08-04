1ab62bcb859f8d1414c1be0021692b6f9af28fb4
Alex Böhm <tools@boehm.work>
2024-08-04 20:24:21 +0200

    ci(eslint): try ESM only

--- packages/eslint-config/tsup.config.ts
+++ packages/eslint-config/tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-	entry: ['src/index.ts', 'src/cli.ts'],
-	shims: true,
+	dts: true,
+	entry: ['src/index.ts'],
+	format: ['esm'],
 });
