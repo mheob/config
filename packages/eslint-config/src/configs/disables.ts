80f69f3fdd64478a993cacf25ff85a4e42288ab4
Alex Böhm <tools@boehm.work>
2025-02-09 16:31:51 +0100

    feat(eslint): disable some eslint rules (#229)
    Resolves #185
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Chores**
    - Upgraded Node.js and various development dependencies to ensure
    improved performance and compatibility.
    - **Refactor**
    - Streamlined linting and formatting configurations to resolve rule
    conflicts and promote consistent code quality.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- packages/eslint-config/src/configs/disables.ts
+++ packages/eslint-config/src/configs/disables.ts
@@ -4,8 +4,24 @@ import type { TypedFlatConfigItem } from '../types';
 export async function disables(): Promise<TypedFlatConfigItem[]> {
 	return [
 		{
-			files: [`**/scripts/${GLOB_SRC}`],
-			name: 'antfu/disables/scripts',
+			files: ['**/bin/**/*', `**/bin.${GLOB_SRC_EXT}`],
+			name: 'mheob/disables/bin',
+			rules: {
+				'antfu/no-import-dist': 'off',
+				'antfu/no-import-node-modules-by-path': 'off',
+			},
+		},
+		{
+			files: [`**/*.${GLOB_SRC_EXT}`],
+			name: 'mheob/disables/conflicts',
+			rules: {
+				'import/order': 'off', // conflicts with `perfectionist/sort-imports`
+				'sort-imports': 'off', // conflicts with `perfectionist/sort-imports`
+			},
+		},
+		{
+			files: [`**/*.config.${GLOB_SRC_EXT}`, `**/*.config.*.${GLOB_SRC_EXT}`],
+			name: 'mheob/disables/config-files',
 			rules: {
 				'antfu/no-top-level-await': 'off',
 				'no-console': 'off',
@@ -14,23 +30,22 @@ export async function disables(): Promise<TypedFlatConfigItem[]> {
 		},
 		{
 			files: [`**/cli/${GLOB_SRC}`, `**/cli.${GLOB_SRC_EXT}`],
-			name: 'antfu/disables/cli',
+			name: 'mheob/disables/cli',
 			rules: {
 				'antfu/no-top-level-await': 'off',
 				'no-console': 'off',
 			},
 		},
 		{
-			files: ['**/bin/**/*', `**/bin.${GLOB_SRC_EXT}`],
-			name: 'antfu/disables/bin',
+			files: ['**/*.js', '**/*.cjs'],
+			name: 'mheob/disables/cjs',
 			rules: {
-				'antfu/no-import-dist': 'off',
-				'antfu/no-import-node-modules-by-path': 'off',
+				'ts/no-require-imports': 'off',
 			},
 		},
 		{
 			files: ['**/*.d.?([cm])ts'],
-			name: 'antfu/disables/dts',
+			name: 'mheob/disables/dts',
 			rules: {
 				'eslint-comments/no-unlimited-disable': 'off',
 				'import/no-duplicates': 'off',
@@ -39,15 +54,8 @@ export async function disables(): Promise<TypedFlatConfigItem[]> {
 			},
 		},
 		{
-			files: ['**/*.js', '**/*.cjs'],
-			name: 'antfu/disables/cjs',
-			rules: {
-				'ts/no-require-imports': 'off',
-			},
-		},
-		{
-			files: [`**/*.config.${GLOB_SRC_EXT}`, `**/*.config.*.${GLOB_SRC_EXT}`],
-			name: 'antfu/disables/config-files',
+			files: [`**/scripts/${GLOB_SRC}`],
+			name: 'mheob/disables/scripts',
 			rules: {
 				'antfu/no-top-level-await': 'off',
 				'no-console': 'off',
