d1c4bcb3b1c22cc10940c36887fb0418fd653704
Alex Böhm <tools@boehm.work>
2025-05-01 12:43:32 +0200

    fix(eslint,deps): update dependencies

--- packages/eslint-config/src/cli/versions-map.generated.ts
+++ packages/eslint-config/src/cli/versions-map.generated.ts
@@ -1,11 +1,11 @@
 export const versionsMap = {
-	'@eslint-react/eslint-plugin': '^1.46.0',
+	'@eslint-react/eslint-plugin': '^1.48.5',
 	'astro-eslint-parser': '^1.2.2',
 	eslint: 'catalog:',
 	'eslint-plugin-astro': '^1.3.1',
 	'eslint-plugin-react-hooks': '^5.2.0',
-	'eslint-plugin-react-refresh': '^0.4.19',
+	'eslint-plugin-react-refresh': '^0.4.20',
 	'eslint-plugin-svelte': '^3.5.1',
 	'prettier-plugin-astro': '^0.14.1',
-	'svelte-eslint-parser': '^1.1.2',
+	'svelte-eslint-parser': '^1.1.3',
 };
