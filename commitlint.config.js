d67569f286122e91efd46f0402951a60046c8b36
Alex Böhm <tools@alex-boehm.dev>
2026-08-03 18:42:21 +0200

    fix(deps): update workspace dependencies

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,7 +9,7 @@ function getScopes() {
 	const defaultScopes = ['deps', 'release', 'repo'];
 	const packagesPath = path.resolve(currentPath, 'packages');
 	const packages = existsSync(packagesPath)
-		? readdirSync(packagesPath).map(packageName => packageName.replace('-config', ''))
+		? readdirSync(packagesPath).map((packageName) => packageName.replace('-config', ''))
 		: [];
 	return [...defaultScopes, ...packages];
 }
